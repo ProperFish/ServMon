29ea3a2bc23f55aad0d160454fb8580a5788d445
Jakob Hansen <Jakob Hansen>
2016-08-31 00:34:58 +0200

    Initial commit...

---
+++ Client/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Client
+    {
+        public static TcpClient tcpClient;
+        public static ASCIIEncoding ascii;
+
+        static void Main(string[] args)
+        {
+            tcpClient = new TcpClient();
+            ascii = new ASCIIEncoding();
+            Console.WriteLine("ServMon Client Software...\n");
+            Console.ReadKey();
+            Console.WriteLine("[INF] Client connection initializing...");
+            tcpClient.Connect("127.0.0.1", 2254);
+            Console.WriteLine("[RDY] Connected!");
+            Console.Write("[INP] Input: ");
+            String str = Console.ReadLine();
+
+            Stream stm = tcpClient.GetStream();
+            byte[] ba = ascii.GetBytes(str);
+
+            Console.WriteLine("[INF] Sending string '{0}' to server...", str);
+
+            stm.Write(ba, 0, ba.Length);
+
+            tcpClient.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        public static TcpListener tcpList;
+        public static Socket tcpSocket;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ServMon Server Software...\n");
+            Console.ReadKey();
+            Console.WriteLine("[INF] Server listener initializing...");
+            InitListener("127.0.0.1",2254);
+            tcpList.Start();
+            Console.WriteLine("[INF] Server is now listening at port {0}/TCP!", tcpList.LocalEndpoint);
+            Console.WriteLine("[RDY] Waiting for connection...");
+
+            tcpSocket = tcpList.AcceptSocket();
+            Console.WriteLine("[INF] Connection attempt from {0}...", tcpSocket.RemoteEndPoint);
+            Console.WriteLine("[OK ] Connected - awaiting commands.");
+
+            byte[] packet = new byte[100];
+            int packetlength = tcpSocket.Receive(packet);
+            Console.WriteLine("[INF] Receiving package from client at {0}", tcpSocket.RemoteEndPoint);
+
+            Console.Write("[MSG] '");
+            for (int i = 0; i < packetlength; i++)
+            {
+                Console.Write(Convert.ToChar(packet[i]));
+            }
+            Console.Write("'\n");
+            
+            tcpSocket.Close();
+            tcpList.Stop();
+            Console.ReadKey();
+        }
+
+        static void InitListener(string ip, int port)
+        {
+            IPAddress ipAddr;
+            ipAddr = IPAddress.Parse(ip);
+            tcpList = new TcpListener(ipAddr, port);
+        }
+    }
+}
