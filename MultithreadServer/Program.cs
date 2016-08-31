e20cd504fe8db1ac3cbcc1ce9ad87acdceb9b47a
Jakob Hansen <Jakob Hansen>
2016-09-01 00:38:36 +0200

    Imported some MSDN code to play around with some more realistic network transmission / client maintaining.

---
+++ MultithreadServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadClient
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            SocketListener.StartListening("127.0.0.1", 2254);
+            return 0;
+        }
+    }
+}
